8e9e339a5e9049ff3dbdbcc0740a84d38660b2d7
zzfn <44751363+zzfn@users.noreply.github.com>
2023-09-12 00:52:21 +0800

    chore: improve

--- main.go
+++ main.go
@@ -5,9 +5,8 @@ import (
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 	"math"
 	"os"
-	"path"
+	"path/filepath"
 	"strconv"
-	"strings"
 )
 
 func BytesToSize(bytes int64) string {
@@ -19,17 +18,30 @@ func BytesToSize(bytes int64) string {
 	return strconv.FormatFloat(float64(bytes)/math.Pow(1024, float64(i)), 'f', 2, 64) + " " + sizes[i]
 }
 
-func getAllFile(dirPath string, parentPath string) (allFiles []string) {
-	files, _ := os.ReadDir(dirPath)
+func getAllFile(dirPath string, parentPath string) ([]string, error) {
+	var allFiles []string
+	files, err := os.ReadDir(dirPath)
+	if err != nil {
+		return nil, err
+	}
 	for _, f := range files {
 		if f.IsDir() {
-			subFlies := getAllFile(dirPath+string(os.PathSeparator)+f.Name(), parentPath+string(os.PathSeparator)+f.Name())
+			subFlies, err := getAllFile(filepath.Join(dirPath, f.Name()), filepath.Join(parentPath, f.Name()))
+			if err != nil {
+				return nil, err
+			}
 			allFiles = append(allFiles, subFlies...)
 		} else {
-			allFiles = append(allFiles, parentPath+string(os.PathSeparator)+f.Name())
+			allFiles = append(allFiles, filepath.Join(parentPath, f.Name()))
 		}
 	}
-	return allFiles
+	return allFiles, nil
+}
+func exitOnError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 func main() {
 	endpoint := os.Getenv("PLUGIN_ENDPOINT")
@@ -40,51 +52,23 @@ func main() {
 	source := os.Getenv("PLUGIN_SOURCE")
 
 	client, err := oss.New(endpoint, accessKeyID, accessKeySecret)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	exitOnError(err)
 	bucket, err := client.Bucket(bucketName)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-		return
-	}
-	files := getAllFile(source, "")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-		return
-	}
+	exitOnError(err)
+	files, err := getAllFile(source, "")
+	exitOnError(err)
 	filesLen := float64(len(files))
 	for index, file := range files {
-		idx := float64(index+1) * 100
-		fileTarget := path.Join(target, file)
-		fileSource := path.Join(source, file)
-		if strings.HasPrefix(fileTarget, "/") {
-			fileTarget = fileTarget[1:]
-		}
+		percentage := (float64(index+1) / filesLen) * 100
+		fileTarget := filepath.Join(target, file)
+		fileSource := filepath.Join(source, file)
 		// 获取文件大小并转换为直观的大小表示
 		fileInfo, err := os.Stat(fileSource)
-		if err != nil {
-			fmt.Println("Error getting file size:", err)
-			os.Exit(1)
-		}
+		exitOnError(err)
 		humanReadableSize := BytesToSize(fileInfo.Size())
-		fmt.Println("uploading " + fileTarget + " " + strconv.FormatFloat(idx/filesLen, 'f', 2, 64) + "%" + " (" + humanReadableSize + ")")
+		fmt.Println("uploading " + fileTarget + " " + strconv.FormatFloat(percentage, 'f', 2, 64) + "%" + " (" + humanReadableSize + ")")
 		err = bucket.PutObjectFromFile(fileTarget, fileSource)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
+		exitOnError(err)
 	}
 	fmt.Println("uploading complete")
 }
